e5dbbfd2a71a698aa038adf712d933c2076d5167
Carl Zou <carlzoulingxiang>
2022-03-07 21:34:20 -0800

    updated server

--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-from app import app, server
+from app import app
 from layout.time_viz.time_viz_callbacks import update_cumulative_count
 from layout.total_capacity.total_capacity_callbacks import update_capacity
 from layout.models.models_callbacks import update_models
